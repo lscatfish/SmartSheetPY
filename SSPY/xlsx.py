cb11db3137359d31f70c867c9b83f8bfb3ab643b
lscatfish <2561925435@qq.com>
2025-09-25 01:12:08 +0800

    xlsx

--- SSPY/xlsx.py
+++ SSPY/xlsx.py
@@ -6,49 +6,6 @@ from openpyxl.styles import Font, Border, Alignment
 from globalconstants import GlobalConstants as gc
 
 
-# # 加载工作簿（read_only=True 适合大文件，提升性能）
-# wb = load_workbook(r"D:\code\SmartSheetPY\青峰班.xlsx", True, True)
-# # data_only=True 表示读取单元格计算后的值，而非公式
-#
-# # 获取所有工作表名称
-# print(wb.sheetnames)  # 输出: ['Sheet1', 'Sheet2']
-# sns = wb.sheetnames
-#
-# # 选择工作表
-# sheet = wb[sns[0]]  # 通过名称选择
-#
-# # 获取工作表维度（数据范围）
-# # print(sheet.dimensions)  # 输出: A1:C5
-# # 读取单元格
-# cell = sheet['A1']
-# print(f"单元格值: {cell.value}")
-# print(f"单元格坐标: {cell.coordinate}")
-# print(f"行号: {cell.row}, 列号: {cell.column}")
-# # 按行读取数据
-# for row in sheet.iter_rows( values_only = True):
-#     for cell in row:
-#         if cell is not None:print(cell)
-#         else:print("i")
-# # 关闭工作簿
-# wb.close()
-
-# wb = Workbook()
-# ws = wb.active
-# content = [('we', 'fhds', 'fjjf'),
-#            ('df', 'dfdsafds', 'dfasd', 'dfsdafsda', 'dsfsad'),
-#            ('dsafdasf', 'dfsda', 'dfrgioehbn'),
-#            ['dfadjonf', 'dfnfd', 'dsfnsnj', 'dsnfiu']]
-# for row in content:
-#     ws.append(row)
-# for i in range(1, len(content) + 1):
-#     ws.row_dimensions[i].height = i * 5 + 20
-# for i in range(1, ws.max_column + 1):
-#     col_letter=ws.cell(row=1, column=i).column_letter
-#     ws.column_dimensions[col_letter].width = i * 5 + 20
-# wb.save('text.xlsx')
-# wb.close()
-
-
 class XlsxLoad:
     """读取xlsx文件的类"""
 
