4cd28f2171dde31749effc07f4f6dd0ef1271163
lscatfish <2561925435@qq.com>
2025-09-25 18:17:02 +0800

    pdf

--- SSPY/helperfunction.py
+++ SSPY/helperfunction.py
@@ -9,3 +9,18 @@ def clean_enter(in_list: list | tuple) -> list:
             nc = c.replace('\n', '').replace('\r', '')
             out_list.append(nc)
     return out_list
+
+
+def check_value(in_list: list | tuple, target_value = None, part: bool = False) -> bool:
+    if target_value is None: return True
+    if target_value not in in_list:
+        for cell in in_list:
+            if isinstance(cell, list | tuple):
+                if check_value(cell, target_value,part): return True
+            else:
+                if part and target_value in cell:
+                    return True
+                continue
+    else:
+        return True
+    return False
--- SSPY/pdf.py
+++ SSPY/pdf.py
@@ -1,14 +1,6 @@
 import pdfplumber
 from helperfunction import *
 
-with pdfplumber.open('./组织推荐班委-李炘宇.pdf') as pdf:
-    page = pdf.pages[0]
-    tables = page.extract_tables()
-    for table in tables:
-        af_table = clean_enter(table)
-        for row in af_table:
-            print(row)
-
 
 class PdfLoad:
     """用于读取pdf文件"""
@@ -19,3 +11,40 @@ class PdfLoad:
         table_only: bool = True):
         self.__path = pdf_path
         self.__tableOnly = table_only
+        self.__sheets = []
+        if self.__tableOnly:
+            self.__extract_tables()
+
+    @property
+    def path(self):
+        return self.__path
+
+    @path.setter
+    def path(self, path: str):
+        self.__path = path
+
+    @property
+    def sheets(self):
+        return self.__sheets
+
+    def get_sheet(self, index = None):
+        if isinstance(index, int):
+            if len(self.__sheets) > index:
+                return self.__sheets[index]
+        if isinstance(index, str):  # 按照关键值查找sheet
+            for sheet in self.__sheets:
+                if check_value(in_list = sheet, target_value = index, part = False): return sheet
+        return None
+
+    def __extract_tables(self):
+        with pdfplumber.open(self.__path) as mypdf:
+            if len(mypdf.pages) <= 0: return False
+            tables = []
+            for page in mypdf.pages:
+                tables.append(page.extract_tables())
+            self.__sheets = clean_enter(tables)
+        return True
+
+
+a = PdfLoad('./组织推荐班委-李炘宇.pdf')
+print(a.get_sheet('姓'))
