11810d4f7a29e01fe73e07996a8115d38e4f9edf
lscatfish <2561925435@qq.com>
2025-10-01 23:24:33 +0800

    临时保存一下，可使用

--- SmartSheetPY.py
+++ SmartSheetPY.py
@@ -1,46 +1,12 @@
-﻿import io
+﻿import paddlex_hijack
+
+import io
 import os
 import sys
-
-
-
 
 os.system('chcp 65001')
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding = 'utf-8', line_buffering = True)
 sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding = 'utf-8', line_buffering = True)
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-os.environ['PADDLE_PDX_CACHE_HOME'] = BASE_DIR
-
-import os, pathlib
-from paddlex.inference.utils.official_models import _ModelManager, _BosModelHoster
-from paddlex.utils import logging
-
-logging.debug = True      # 打开 DEBUG
-
-# 1. 本地镜像根目录（自己维护）
-PRIVATE_MIRROR = pathlib.Path(os.path.join(BASE_DIR, 'official_models'))
-
-# 3. 兜底下载器（复用官方 BOS 源）
-_bos_hoster = _BosModelHoster(PRIVATE_MIRROR)
-
-# 2. 彻底替换 _get_model_local_path：永远不走 hoster
-def _offline_get_model_local_path(self, model_name):
-    local_dir = PRIVATE_MIRROR / model_name
-    if local_dir.exists():
-        return local_dir
-    # ② 镜像缺失 → 走官方下载（会自动保存到 CACHE_DIR）
-    #logging.info(f'本地镜像缺失，正在从官方源下载 {model_name} …')
-    return _bos_hoster.get_model(model_name)  # 会调用 _download → 官方地址
-
-
-
-# 3. 热补丁
-_ModelManager._get_model_local_path = _offline_get_model_local_path
-
-# 4. 可选：把 save_dir 也指向同一目录，防止框架再创建默认路径
-_ModelManager._save_dir = PRIVATE_MIRROR
-
 
 
 from QingziClass.doqingziclass import DoQingziClass
--- paddlex_hijack.py
+++ paddlex_hijack.py
@@ -11,7 +11,7 @@ from paddlex.inference.utils.official_models import _ModelManager, _BosModelHost
 
 # ========== 1. 你要放模型的根目录 ==========
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-MY_MIRROR_ROOT = pathlib.Path(BASE_DIR)  # 改成你的路径
+MY_MIRROR_ROOT = pathlib.Path(os.path.join(BASE_DIR, 'official_models'))  # 改成你的路径
 os.environ["PADDLE_PDX_CACHE_HOME"] = BASE_DIR
 MY_MIRROR_ROOT.mkdir(parents = True, exist_ok = True)
 
@@ -21,10 +21,11 @@ class _OfflineBosHoster(_BosModelHoster):
     """本地有就用，没有就抛异常，绝不下载"""
 
     def _download(self, model_name: str, save_dir: pathlib.Path):
-        raise FileNotFoundError(
-            f"本地镜像缺失：{model_name} ，已禁止自动下载！\n"
-            f"请将完整模型文件夹手动放到 {save_dir} 然后重试。"
-        )
+        super()._download(model_name, save_dir)
+        # raise FileNotFoundError(
+        #     f"本地镜像缺失：{model_name} ，已禁止自动下载！\n"
+        #     f"请将完整模型文件夹手动放到 {save_dir} 然后重试。"
+        # )
 
 
 # ========== 3. 热替换 _ModelManager 的核心函数 ==========
@@ -43,5 +44,3 @@ def _hijacked_get_model_local_path(self, model_name: str) -> pathlib.Path:
 # ========== 4. 注入补丁 ==========
 _ModelManager._get_model_local_path = _hijacked_get_model_local_path
 _ModelManager._save_dir = MY_MIRROR_ROOT  # 顺带把默认目录也改掉
-
-
