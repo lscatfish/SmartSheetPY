d0267dd65d8e2d9b6b1d7061e1bbfc9a883509f0
lscatfish <2561925435@qq.com>
2025-10-02 15:14:39 +0800

    新增智能搜索函数

--- SSPY/fuzzy/search.py
+++ SSPY/fuzzy/search.py
@@ -1,4 +1,5 @@
 """模糊搜索模块"""
+import copy
 from enum import Enum, unique
 
 
@@ -62,34 +63,28 @@ class LEVEL(Enum):
     Unknow = 1000
 
 
+@unique
+class Priority(Enum):
+    """搜索优先级"""
+    Time = 0
+    """时间优先(最快)"""
+    Similarity = 1
+    """相似度优先"""
+
+
 def match_by(a: str, b: str, level = LEVEL.Perfect):
-    if level == LEVEL.Perfect:
-        return a == b
-    distance = levenshtein_distance(a, b)
-    mp = level.value
-    return distance <= mp
-
-
-def search_in(target: str, lib: list[str], level = LEVEL.Perfect):
-    """返回搜索到的表"""
-    out_list = []
-    for a in lib:
-        if match_by(target, a, level):
-            out_list.append(a)
-    return out_list
-
-
-def searched(target: str, lib: list[str], level = LEVEL.Perfect) -> bool:
-    """返回是否搜索到"""
-    for a in lib:
-        if match_by(target, a, level):
-            return True
+    if isinstance(a, str) and isinstance(b, str):
+        if level == LEVEL.Perfect:
+            return a == b
+        distance = levenshtein_distance(a, b)
+        mp = level.value
+        return distance <= mp
     return False
 
 
-def searched_auto(
+def searched_recursive(
     target: str,
-    lib: tuple | list | str,
+    lib: tuple | list | str | dict,
     level = LEVEL.Perfect,
     target_as_sub: bool = False,
     lib_as_sub: bool = False):
@@ -108,13 +103,149 @@ def searched_auto(
     if not isinstance(target, str): return False
     if isinstance(lib, list | tuple):
         for r in lib:
-            if searched_auto(target, r, level, target_as_sub, lib_as_sub):
+            if searched_recursive(target, r, level, target_as_sub, lib_as_sub):
+                return True
+    elif isinstance(lib, dict):
+        for k in lib.keys():
+            if searched_recursive(target, k, level, target_as_sub, lib_as_sub):
+                return True
+            if searched_recursive(target, lib[k], level, target_as_sub, lib_as_sub):
                 return True
     elif isinstance(lib, str):
-        if match_by(target, lib, level):
-            return True
+        if match_by(target, lib, level): return True
         if target_as_sub:
             if target in lib: return True
         if lib_as_sub:
             if lib in target: return True
     return False
+
+
+def search_recursive(
+    target: str,
+    lib: tuple | list | str | dict,
+    level = LEVEL.Perfect,
+    target_as_sub: bool = False,
+    lib_as_sub: bool = False):
+    """
+    对任意列表进行递归搜索，支持模糊
+    Args:
+        target:搜索目标
+        lib:搜索的源
+        level:搜索匹配度等级
+        target_as_sub:将目标视作字串搜索
+        lib_as_sub:将库字符视作字串搜索
+    Returns:
+        满足条件搜索值的列表
+    """
+    ms: list[str] = []
+    if target is None: return ms
+    if not isinstance(target, str): return ms
+    if isinstance(lib, list | tuple):
+        for r in lib:
+            ms.extend(search_recursive(target, r, level, target_as_sub, lib_as_sub))
+    elif isinstance(lib, dict):
+        for k in lib.keys():
+            ms.extend(search_recursive(target, k, level, target_as_sub, lib_as_sub))
+            ms.extend(search_recursive(target, lib[k], level, target_as_sub, lib_as_sub))
+    elif isinstance(lib, str):
+        if match_by(target, lib, level): ms.append(lib)
+        if target_as_sub:
+            if target in lib: ms.append(lib)
+        if lib_as_sub:
+            if lib in target: ms.append(lib)
+    return ms
+
+
+def search_auto(
+    target: str,
+    lib: tuple | list | str,
+    level = LEVEL.Perfect,
+    target_as_sub: bool = False,
+    lib_as_sub: bool = False, ):
+    """
+    基础函数，智能搜索函数
+    Args:
+        target:搜索目标
+        lib:搜索的源
+        level:搜索匹配度等级
+        target_as_sub:将目标视作字串搜索
+        lib_as_sub:将库字符视作字串搜索
+    Returns:
+        返回一个有序的结果
+        排序规则：
+        1.完全匹配
+        2.target_as_sub
+        3.lib_as_sub
+        3.fuzzy
+    """
+    orgList = search_recursive(target, lib, level, target_as_sub, lib_as_sub)  # 解析出的原始序列
+
+    if len(orgList) <= 1: return orgList
+
+    sortedList: list[str] = []
+    indices: list[int] = []
+
+    def __del_by_indices():
+        nonlocal indices
+        nonlocal orgList
+        if len(orgList) == 0 or len(indices) == 0: return
+        for i in sorted(indices, reverse = True):
+            del orgList[i]
+        indices.clear()
+
+    for index in range(len(orgList)):
+        if match_by(target, orgList[index]):
+            indices.append(index)
+            sortedList.append(orgList[index])
+    __del_by_indices()
+
+    if target_as_sub:
+        for index in range(len(orgList)):
+            if target in orgList[index]:
+                indices.append(index)
+                sortedList.append(orgList[index])
+        __del_by_indices()
+    elif lib_as_sub:
+        for index in range(len(orgList)):
+            if orgList[index] in target:
+                indices.append(index)
+                sortedList.append(orgList[index])
+        __del_by_indices()
+
+    elif level != LEVEL.Perfect:
+        dpMap: list[tuple[str, int]] = []
+        for item in orgList:
+            dpMap.append((item, levenshtein_distance(target, item)))
+        sorteddpMap = sorted(dpMap, key = lambda x: x[1])
+        for item in sorteddpMap:
+            sortedList.append(item[0])
+    return sortedList
+
+
+def search_only(
+    target: str,
+    lib: tuple | list | str,
+    level = LEVEL.Perfect,
+    target_as_sub: bool = False,
+    lib_as_sub: bool = False, ):
+    """
+    基础函数，智能搜索函数
+    Args:
+        target:搜索目标
+        lib:搜索的源
+        level:搜索匹配度等级
+        target_as_sub:将目标视作字串搜索
+        lib_as_sub:将库字符视作字串搜索
+    Returns:
+        返回最有可能的结果
+        排序规则：
+        1.完全匹配
+        2.target_as_sub
+        3.lib_as_sub
+        3.fuzzy
+    """
+    inl = search_auto(target, lib, level, target_as_sub, lib_as_sub)
+    if len(inl) == 0:
+        return None
+    else:
+        return inl[0]
