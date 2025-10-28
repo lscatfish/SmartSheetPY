0537d500a16965c047ee8df3a740f2f654e33448
lscatfish <2561925435@qq.com>
2025-10-28 21:04:34 +0800

    every package is doomed to be shit

--- SSPY/__init__.py
+++ SSPY/__init__.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 """
+every package is doomed to be shit
+
 如果你单独使用此包，我们推荐你劫持ppocr的下载器以及相关子线程
 If you use this package alone, we recommend that you hijack the ppocr downloader and the related subthreads.
 
